e313e51985b5576a9dc0709721304affd448ca1b
vergilyun <95463573+vergilyun@users.noreply.github.com>
2025-06-17 10:30:11 +0800

    Update keyword.ts

--- lib/routes/twitter/keyword.ts
+++ lib/routes/twitter/keyword.ts
@@ -23,7 +23,7 @@ export const route: Route = {
                 description: 'Please see above for details.',
             },
             {
-                name: 'AAAAAAAAAAAAAAAAAAAAAEsi2gEAAAAAoGdCWpplMLgZIpAZwM2KkMgeKd8%3DNLjfxw6DEkGdd8qrAYMs61BIiC3mz69AgOSkEM06gWlfzt1HbJ',
+                name: 'oOjfrKsttHQE7e6ujlRw2tBgS',
                 description: 'Please see above for details.',
             },
         ],
