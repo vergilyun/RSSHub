d4f36592b15d662aa3d081520c7d6dda61e5011e
vergilyun <95463573+vergilyun@users.noreply.github.com>
2025-06-17 14:22:11 +0800

    Update media.ts

--- lib/routes/twitter/media.ts
+++ lib/routes/twitter/media.ts
@@ -12,15 +12,15 @@ export const route: Route = {
     features: {
         requireConfig: [
             {
-                name: 'omniyun',
+                name: omniyun,
                 description: 'Please see above for details.',
             },
             {
-                name: 'qpmzg_13579',
+                name: qpmzg_13579,
                 description: 'Please see above for details.',
             },
             {
-                name: '87682c7fbbabc695b0a8c0d1e94c21f2e810b716',
+                name: 87682c7fbbabc695b0a8c0d1e94c21f2e810b716,
                 description: 'Please see above for details.',
             },
         ],
